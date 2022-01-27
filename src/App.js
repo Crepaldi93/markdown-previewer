8aec55c1dfc457e221d6534adb7fab00fda4382f
Crepaldi93 <96325460+Crepaldi93@users.noreply.github.com>
2022-01-27 12:28:44 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { marked } from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    markdown: `# This is an h1 
+    
+## This is an h2
+    
+[This is a link](https://freecodecamp.org)
+    
+\`<code>This is inline code</code>\`
+    
+    <html>
+      <head>
+        This is a code block
+      </head>
+    </html>
+    
+- This
+- is
+- a
+- list
+    
+> This is a blockquote
+    
+![Watchmen Smiley face](https://b.thumbs.redditmedia.com/WKTbYQf3RyVIegwgSOlc3qy4k6soYLKldWdNVvKkBWQ.png)
+
+    
+**This is bold**
+    `
+  }
+  
+  updateMarkdown = function(markdown){
+    this.setState({markdown});
+  }
+  render() {
+    let { markdown } = this.state;
+    console.log(markdown)
+    return (
+      <div className="App container">
+        <div class="row" id="editor-title">
+          <label class="col">Markdown Input</label>
+        </div>
+        <div class="row">  
+          <textarea id="editor" class="col" placeholder="Enter Markdown" value={markdown} onChange={(event)=>this.updateMarkdown(event.target.value)}></textarea>
+        </div>
+        <div>
+          <h1 id="preview-title">Markdown Output</h1>
+          <div id="preview" dangerouslySetInnerHTML={{__html: marked(markdown, { breaks: true })}} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
